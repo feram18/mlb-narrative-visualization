4f18d0d9279f59af1d012fc69b0b97d4e113c227
Luis Ramos <feram99ag@gmail.com>
2023-07-25 21:46:55 -0400

    implement visualization

---
+++ README.md
@@ -0,0 +1,31 @@
+# Narrative Visualization of MLB Batting Performance
+> UIUC | CS 416 - Data Visualization
+
+## Messaging
+This narrative visualization's purpose is to offer a perceptive examination of Major League Baseball's (MLB) batting statistics ranging from 2015 to 2023. It intends to display the average 'batting average (AVG)' among qualified batters per year in the overview scene, let users dive down into certain years to see the correlation between players' swing percentage and batting AVG, and provide a more in-depth analysis of the performance of specific players.
+
+## Narrative Structure
+Users can switch between many scenes thanks to the interactive slide show format that the narrative visualization implemented in this project. The visualization opens with an overview scenario that uses a bar chart to show the average batting average by year, which highlights a consistent trend over the years. Users can then use a scatter plot to delve further into a particular year to examine batting statistics. Finally, they can choose a specific player to view the distribution of hit types as a bar chart for that player.
+
+## Visual Structure
+Each scene is created to make it easier to understand and navigate the data displayed. A bar chart in the overview scene effectively displays the average 'batting average' statistic over time. Users can dive down into specific years in the interactive slide show, where a scatter plot efficiently illustrates the connection between each player's swing percentage and batting average, a matching color helps indicate the user the year shown in the scatterplot. The scatterplot invites the user to think about whether a player's swing percentage can have an impact on their number of walks, home runs, or strikeouts. A back button is provided to switch between scenes, ensuring easy navigation across the display.
+
+## Scenes
+Three scenes make up the narrative visualization:
+1. Overview Scene: Uses a bar chart to show the average "batting average (AVG)" by year.
+2. Drill-Down Scene: This feature enables users to choose a specific year and observe a scatter plot depicting the correlation between swing percentage and batting average.
+3. Individual Player Scene: Offers a bar chart showing the breakdown of hits types for a chosen player over the course of a particular year.
+
+The scenes flow naturally from a high-level overview of batting statistics to more in-depth analyses of particular years and specific players.
+
+## Annotations
+The callout circle annotation template is used for the annotations, which are intentionally positioned to draw attention to the players who have the most home runs, strikeouts, and walks in the drill-down scene. These annotations serve to reinforce the idea by highlighting the players who led the season in those categories. As users examine the data, the annotations remain constant inside a scenario, giving them reliable visual clues.
+
+## Parameters
+The player and year that are chosen serve as the narrative visualization's parameters. These factors determine the visualization's state and have an impact on the information shown in each scene. Users can explore different years and players by changing the states, which dynamically updates the representations.
+
+## Triggers
+Changes in the narrative visualization's state are brought about by user actions. The drill-down scene for the chosen year is launched by clicking a bar in the overview scene. Users can examine the individual player scene in the drill-down scene by clicking on a data point representing a player, which displays the hits distribution for that player in the selected year. The back button gives users the option to go back to the previous scene.
+
+## Affordances
+The narrative visualization provides users with a number of affordances to help them. The sections that users can interact with are indicated by interactive features, such as the bars and data points, which react to mouse-over and click operations. Additionally, the back button improves navigation and user experience by graphically communicating that users can go back to the previous scene.
--- script.js
+++ script.js
@@ -2,85 +2,441 @@
 let selectedYear = null;
 let selectedPlayer = null;
 
-// Define the URL for the CSV data source
-const dataURL = 'https://raw.githubusercontent.com/feram18/mlb-narrative-visualization/main/mlb-stats.csv'; // Replace with the actual URL
+const dataURL = 'https://raw.githubusercontent.com/feram18/mlb-narrative-visualization/main/mlb-stats.csv';
 
-// Load data from the URL using fetch
 fetch(dataURL)
   .then(response => response.text())
   .then(csvData => {
-    // Parse the CSV data
-    const data = d3.csvParse(csvData);
-
-    // Convert numerical columns to numbers
-    data.forEach(d => {
+    const data = d3.csvParse(csvData, d => {
       d.AB = +d.AB;
       d.PA = +d.PA;
-      // Add more conversions for other numerical columns if needed
+      d.batting_avg = d.batting_avg ? +d.batting_avg : 0;
+      return d;
     });
 
-    // Process the data further as needed
-
-    // Call the main function to create the visualization
     createVisualization(data);
   });
 
-
 function createVisualization(data) {
-  // Define the main SVG container
+  const margin = {
+    top: 70,
+    right: 50,
+    bottom: 80,
+    left: 70
+  };
+  const width = 800 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
+
   const svg = d3.select("#visualization").append("svg")
-    .attr("width", 800)
-    .attr("height", 500);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // Implement the initial overview scene
+  const years = Array.from(new Set(data.map(d => d.year)));
+  const colorScale = d3.scaleOrdinal()
+    .domain(years)
+    .range(d3.schemePaired);
+
   function showOverviewScene() {
-    // Clear the SVG and show the overview scene
     svg.selectAll("*").remove();
 
-    // Add code here to create the overview scene, e.g., bar chart for average AVG by year
-    // Add interaction to trigger the drill-down scene when a year is clicked
+    // Aggregate data for each year
+    const yearAverages = years.map(year => {
+      const yearData = data.filter(d => d.year === year);
+      const avgAVG = d3.mean(yearData, d => d.batting_avg);
+      return {
+        year,
+        avgAVG
+      };
+    });
 
-    // Example: Show a message for the overview scene
-    svg.append("text")
-      .attr("x", 400)
-      .attr("y", 250)
+    // Create the X and Y scales
+    const xScale = d3.scaleBand()
+      .domain(yearAverages.map(d => d.year))
+      .range([0, width])
+      .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(yearAverages, d => d.avgAVG)])
+      .range([height, 0]);
+
+    // Create and display the X-axis
+    const xAxis = d3.axisBottom(xScale);
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis)
+      .selectAll("text")
+      .attr("transform", "rotate(-45)")
+      .attr("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em");
+
+    // Create and display the Y-axis
+    const yAxis = d3.axisLeft(yScale).ticks(6);
+    svg.append("g")
+      .attr("class", "y-axis")
+      .call(yAxis);
+
+    // Create the bars for each year
+    const bars = svg.selectAll(".bar")
+      .data(yearAverages)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", d => xScale(d.year))
+      .attr("y", d => yScale(d.avgAVG))
+      .attr("width", xScale.bandwidth())
+      .attr("height", d => height - yScale(d.avgAVG))
+      .attr("fill", d => colorScale(d.year))
+      .on("click", (event, d) => triggerDrillDown(d.year));
+
+    // Add AVG values at the top of the bars
+    svg.selectAll(".bar-label")
+      .data(yearAverages)
+      .enter()
+      .append("text")
+      .attr("class", "bar-label")
+      .attr("x", d => xScale(d.year) + xScale.bandwidth() / 2)
+      .attr("y", d => yScale(d.avgAVG) - 5)
       .attr("text-anchor", "middle")
-      .text("Overview Scene - Click on a year to drill down");
+      .text(d => d.avgAVG.toFixed(3));
+
+    // Add title
+    svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", -margin.top / 2)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "20px")
+      .text("Average 'Batting Average (AVG)' by Year");
+
+    // Add X and Y axis labels
+    svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", height + margin.bottom / 2)
+      .attr("text-anchor", "middle")
+      .text("Year");
+
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height / 2)
+      .attr("y", -margin.left + 15)
+      .attr("text-anchor", "middle")
+      .text("Average AVG");
   }
 
-  // Implement the drill-down scene
   function showDrillDownScene(year) {
-    // Clear the SVG and show the drill-down scene for the selected year
     svg.selectAll("*").remove();
 
-    // Add code here to create the drill-down scene, e.g., table showing players' AVG for the selected year
-    // Add interaction to allow users to select a player and trigger the individual player scene
+    // Filter the data to get the players for the selected year
+    const yearPlayers = data.filter(d => d.year === year);
 
-    // Example: Show a message for the drill-down scene
+    // Calculate the minimum and maximum values for Swing % and Batting AVG
+    const swingPercentMin = d3.min(yearPlayers, d => d.swing_percent);
+    const swingPercentMax = d3.max(yearPlayers, d => d.swing_percent);
+    const battingAvgMin = d3.min(yearPlayers, d => d.batting_avg);
+    const battingAvgMax = d3.max(yearPlayers, d => d.batting_avg);
+
+    // Add some space to the domain using a buffer
+    const swingBuffer = 3;
+    const battingBuffer = 0.01;
+
+    // Create the X and Y scales for Swing % and Batting AVG with buffer
+    const xScale = d3.scaleLinear()
+      .domain([Math.max(0, swingPercentMin - swingBuffer), swingPercentMax + swingBuffer])
+      .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+      .domain([Math.max(0, battingAvgMin - battingBuffer), battingAvgMax + battingBuffer])
+      .range([height, 0]);
+
+    // Create and display the X-axis for Swing %
+    const xAxis = d3.axisBottom(xScale).ticks(6);
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    // Create and display the Y-axis for Batting AVG
+    const yAxis = d3.axisLeft(yScale).ticks(6);
+    svg.append("g")
+      .attr("class", "y-axis")
+      .call(yAxis);
+
+    const pointGroup = svg.append("g")
+      .attr("class", "point-group");
+
+    // Create the scatter plot points for each player
+    const points = pointGroup.selectAll(".point")
+      .data(yearPlayers)
+      .enter()
+      .append("circle")
+      .attr("class", "point")
+      .attr("cx", d => xScale(d.swing_percent))
+      .attr("cy", d => yScale(d.batting_avg))
+      .attr("r", 5)
+      .attr("fill", d => colorScale(year))
+      .on("mouseover", showPlayerName)
+      .on("mouseout", hidePlayerName)
+      .on("click", (event, d) => triggerShowPlayer(d));
+
+    function showPlayerName(event, d) {
+      const xPosition = xScale(d.swing_percent) + 10;
+      const yPosition = yScale(d.batting_avg) - 5;
+
+      svg.append("text")
+        .attr("class", "annotation-text")
+        .attr("x", xPosition)
+        .attr("y", yPosition)
+        .attr("dy", "0.35em")
+        .text(`${d.first_name} ${d.last_name}`);
+    }
+
+    function hidePlayerName() {
+      svg.select(".annotation-text").remove();
+    }
+
+    // Add chart title
     svg.append("text")
-      .attr("x", 400)
-      .attr("y", 250)
+      .attr("x", width / 2)
+      .attr("y", -margin.top / 2)
       .attr("text-anchor", "middle")
-      .text(`Drill-Down Scene for Year ${year}`);
+      .attr("font-size", "20px")
+      .text(`Batting Statistics for ${year}`);
+
+    // Add X and Y axis labels
+    svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", height + margin.bottom / 2)
+      .attr("text-anchor", "middle")
+      .text("Swing %");
+
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height / 2)
+      .attr("y", -margin.left + 15)
+      .attr("text-anchor", "middle")
+      .text("Batting AVG");
+
+    // Add Back button to go back to the Overview scene
+    const backButton = svg.append("g")
+      .attr("class", "back-button")
+      .attr("transform", `translate(10, ${-margin.top / 2})`)
+      .style("cursor", "pointer")
+      .on("click", triggerBackToOverview);
+
+    backButton.append("rect")
+      .attr("width", 60)
+      .attr("height", 25)
+      .attr("fill", "lightgray")
+      .attr("rx", 5);
+
+    backButton.append("text")
+      .attr("x", 30)
+      .attr("y", 15)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "12px")
+      .text("< Overview");
+
+    const maxHomeRunsPlayer = getMaxPlayer(yearPlayers, "home_run");
+    const maxWalksPlayer = getMaxPlayer(yearPlayers, "walk");
+    const maxStrikeoutsPlayer = getMaxPlayer(yearPlayers, "strikeout");
+  
+    function getMaxPlayer(players, stat) {
+      // Sort the players in descending order based on the specified stat
+      const sortedPlayers = players.slice().sort((a, b) => b[stat] - a[stat]);
+      // Get the player with the highest value for the specified stat
+      return sortedPlayers[0];
+    }
+
+    const annotations = [
+      {
+        note: {
+          title: `Most Home Runs: ${maxHomeRunsPlayer.home_run}`,
+          label: `${maxHomeRunsPlayer.first_name} ${maxHomeRunsPlayer.last_name}`,
+          wrap: 190,
+        },
+        connector: {
+          end: "dot",
+          type: "line",
+        },
+        data: { swing_percent: maxHomeRunsPlayer.swing_percent, batting_avg: maxHomeRunsPlayer.batting_avg },
+        dy: -30,
+        dx: -30,
+        subject: {
+          radius: 5,
+        },
+        align: "dynamic",
+      },
+    
+      {
+        note: {
+          title: `Most Strikeouts: ${maxStrikeoutsPlayer.strikeout}`,
+          label: `${maxStrikeoutsPlayer.first_name} ${maxStrikeoutsPlayer.last_name}`,
+          wrap: 190,
+        },
+        connector: {
+          end: "dot",
+          type: "line",
+        },
+        data: { swing_percent: maxStrikeoutsPlayer.swing_percent, batting_avg: maxStrikeoutsPlayer.batting_avg },
+        dy: 30,
+        dx: -30,
+        subject: {
+          radius: 5,
+        },
+        align: "dynamic",
+      },
+    
+      {
+        note: {
+          title: `Most Walks: ${maxWalksPlayer.walk}`,
+          label: `${maxWalksPlayer.first_name} ${maxWalksPlayer.last_name}`,
+          wrap: 190,
+        },
+        connector: {
+          end: "dot",
+          type: "line",
+        },
+        data: { swing_percent: maxWalksPlayer.swing_percent, batting_avg: maxWalksPlayer.batting_avg },
+        dy: -30,
+        dx: 30,
+        subject: {
+          radius: 5,
+        },
+        align: "dynamic",
+      },
+    ];
+
+    const makeAnnotations = d3.annotation()
+      .annotations(annotations)
+      .type(d3.annotationCalloutCircle)
+      .accessors({
+        x: d => xScale(d.swing_percent),
+        y: d => yScale(d.batting_avg),
+      })
+      .accessorsInverse({
+        swing_percent: d => xScale.invert(d.x),
+        batting_avg: d => yScale.invert(d.y),
+      })
+      .textWrap(190)
+      .notePadding(10);
+
+    svg.append("g")
+      .attr("class", "annotation-group")
+      .call(makeAnnotations);
   }
 
-  // Implement the individual player scene
-  function showIndividualPlayerScene(player) {
-    // Clear the SVG and show the scene for the selected player
+  function showIndividualPlayerScene(year, player) {
     svg.selectAll("*").remove();
 
-    // Add code here to create the individual player scene, e.g., line chart showing player's AVG over time
-    // Add interaction to allow users to navigate back to the drill-down scene or overview scene
+    // Hits distribution
+    const hitsData = [{
+        type: "Single",
+        count: player.single
+      },
+      {
+        type: "Double",
+        count: player.double
+      },
+      {
+        type: "Triple",
+        count: player.triple
+      },
+      {
+        type: "Home Run",
+        count: player.home_run
+      },
+    ];
 
-    // Example: Show a message for the individual player scene
-    svg.append("text")
-      .attr("x", 400)
-      .attr("y", 250)
+    const xScale = d3.scaleBand()
+      .domain(hitsData.map(d => d.type))
+      .range([0, width])
+      .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, Math.max(player.single, player.double, player.triple, player.home_run)])
+      .range([height, 0]);
+
+    // Create and display axes
+    const xAxis = d3.axisBottom(xScale);
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    const yAxis = d3.axisLeft(yScale).ticks(6);
+    svg.append("g")
+      .attr("class", "y-axis")
+      .call(yAxis);
+
+    // Create the bars for the hits distribution chart
+    const bars = svg.selectAll(".bar")
+      .data(hitsData)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", d => xScale(d.type))
+      .attr("y", d => yScale(d.count))
+      .attr("width", xScale.bandwidth())
+      .attr("height", d => height - yScale(d.count))
+      .attr("fill", "steelblue");
+
+    // Add count labels
+    svg.selectAll(".bar-label")
+      .data(hitsData)
+      .enter()
+      .append("text")
+      .attr("class", "bar-label")
+      .attr("x", d => xScale(d.type) + xScale.bandwidth() / 2)
+      .attr("y", d => yScale(d.count) - 5)
       .attr("text-anchor", "middle")
-      .text(`Individual Player Scene for ${player}`);
+      .text(d => d.count);
+
+    // Add title
+    svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", -margin.top / 2)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "20px")
+      .text(`${player.first_name} ${player.last_name} Batting Statistics in ${player.year}`);
+
+    // Add X and Y axis labels for hits distribution chart
+    svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", height + margin.bottom / 2)
+      .attr("text-anchor", "middle")
+      .text("Hits Type");
+
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height / 2)
+      .attr("y", -margin.left + 15)
+      .attr("text-anchor", "middle")
+      .text("Number of Hits");
+
+    // Add Back button to go back to the drill-down scene
+    const backButton = svg.append("g")
+      .attr("class", "back-button")
+      .attr("transform", `translate(10, ${-margin.top / 2})`)
+      .style("cursor", "pointer")
+      .on("click", triggerBackToDrillDown);
+
+    backButton.append("rect")
+      .attr("width", 60)
+      .attr("height", 25)
+      .attr("fill", "lightgray")
+      .attr("rx", 5);
+
+    backButton.append("text")
+      .attr("x", 30)
+      .attr("y", 15)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "12px")
+      .text("Back");
   }
 
-  // Define triggers for user interactions
   function triggerDrillDown(year) {
     selectedYear = year;
     showDrillDownScene(year);
@@ -88,7 +444,7 @@ function createVisualization(data) {
 
   function triggerShowPlayer(player) {
     selectedPlayer = player;
-    showIndividualPlayerScene(player);
+    showIndividualPlayerScene(selectedYear, player);
   }
 
   function triggerBackToDrillDown() {
@@ -101,6 +457,5 @@ function createVisualization(data) {
     showOverviewScene();
   }
 
-  // Show the initial overview scene
   showOverviewScene();
 }
